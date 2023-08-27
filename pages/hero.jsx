a3aab2bd8ec859bf96f69bdf32f9ee799b4ca310
Abhinav K <62065710+abhinavk001@users.noreply.github.com>
2023-08-27 20:08:37 +0000

    navbar

---
+++ pages/hero.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/component/Navbar";
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Hero;
